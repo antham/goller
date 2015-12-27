d68560563ac8d4051800096d9a587e98b0aff6e5
antham <hamonanth@gmail.com>
2015-12-27 23:33:26 +0100

    ref(reader) : use stdin instead of reading file

--- reader/reader.go
+++ reader/reader.go
@@ -2,37 +2,21 @@ package reader
 
 import (
 	"bufio"
-	"log"
+	"io"
 	"os"
-	"path/filepath"
 )
 
-var filenames []string
+var input io.Reader
 
-// Set init filenames to use
-func Set(pattern string) {
-	var err error
-
-	filenames, err = filepath.Glob(pattern)
-
-	if err != nil {
-		log.Fatalf("An error occured when retrieving filenames from pattern : %s", pattern)
-	}
+//init initialize variables
+func init() {
+	input = os.Stdin
 }
 
-func Read(rowReader func(line string)) {
-	for _, filename := range filenames {
-		pointer, err := os.Open(filename)
-		defer pointer.Close()
-
-		if err != nil {
-			log.Fatalf("Can't open %s", filename)
-		}
-
-		scanner := bufio.NewScanner(pointer)
-
-		for scanner.Scan() {
-			rowReader(scanner.Text())
-		}
+//ReadStdin split stdin per line
+func ReadStdin(rowReader func(line string)) {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		rowReader(scanner.Text())
 	}
 }
---
+++ reader/reader_test.go
@@ -0,0 +1,28 @@
+package reader
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestReadStdin(t *testing.T) {
+	input = strings.NewReader("test1\ntest2\ntest3")
+
+	entries := []string{
+		"test1",
+		"test2",
+		"test3",
+	}
+
+	ReadStdin(func(line string) {
+		expected := entries[0]
+
+		if expected != line {
+			t.Errorf("Line must be %s, got %s", expected, line)
+		}
+
+		if len(entries) > 0 {
+			entries = entries[1:]
+		}
+	})
+}
