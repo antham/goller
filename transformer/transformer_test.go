664ab149bfa972bec0d18ca4e60f63b9e1e778d1
antham <hamonanth@gmail.com>
2016-01-07 23:33:16 +0100

    test(transformer) : split

--- transformer/transformer_test.go
+++ transformer/transformer_test.go
@@ -4,14 +4,26 @@ import (
 	"testing"
 )
 
-func TestAppend(t *testing.T) {
+func TestLow(t *testing.T) {
 	transformers := &Transformers{}
 
-	transformers.Append("upp", []string{})
 	transformers.Append("low", []string{})
 
 	result := transformers.Apply("A RANDOM TEST")
 	expected := "a random test"
+
+	if result != expected {
+		t.Errorf("%s got %s", expected, result)
+	}
+}
+
+func TestUpp(t *testing.T) {
+	transformers := &Transformers{}
+
+	transformers.Append("upp", []string{})
+
+	result := transformers.Apply("a random test")
+	expected := "A RANDOM TEST"
 
 	if result != expected {
 		t.Errorf("%s got %s", expected, result)
